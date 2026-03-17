040d36203bfeaa4b1efbdcfa48ccca127fd71192
per <per@alipsa.se>
2026-03-17 19:26:00 +0100

    update release scripts

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ -n "$(git status --porcelain)" ]; then
   echo "Error: Git working tree is not clean. Commit or stash changes before publishing."
   exit 1
@@ -6,5 +7,7 @@ fi
 if command -v jdk17 &> /dev/null; then
   echo "Found jdk17 command, executing it..."
   . jdk17
+  export PATH="$JAVA_HOME/bin:$PATH"
 fi
-./gradlew clean build publishPlugins
+java -version
+#./gradlew clean build publishPlugins
--- validate.sh
+++ validate.sh
@@ -2,6 +2,7 @@
 if command -v jdk17 &> /dev/null; then
   echo "Found jdk17 command, executing it..."
   . jdk17
+  export PATH="$JAVA_HOME/bin:$PATH"
 fi
 ./gradlew build
 ./gradlew publishPlugins --validate-only
